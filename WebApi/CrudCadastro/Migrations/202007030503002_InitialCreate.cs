81b290ea9c986e69793e5d34c1fe31fcccda6e15
di3go5g <diego.luis.gomes2015@gmail.com>
2020-07-03 14:44:15 -0300

    chore: change folder

---
+++ WebApi/CrudCadastro/Migrations/202007030503002_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿namespace CrudCadastro.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.UsuarioCadastroes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(nullable: false, maxLength: 50),
+                        SobreNome = c.String(nullable: false, maxLength: 100),
+                        Email = c.String(nullable: false, maxLength: 100),
+                        DataNascimento = c.DateTime(nullable: false),
+                        Escolaridade = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.UsuarioCadastroes");
+        }
+    }
+}
---
+++ WebApi/CrudCadastro/Models/UsuarioCadastro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CrudCadastro.Models
+{
+    public class UsuarioCadastro
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Nome { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string SobreNome { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Email { get; set; }
+        [Required]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime DataNascimento { get; set; }
+        [Required]
+        public int Escolaridade { get; set; }
+    }
+}
